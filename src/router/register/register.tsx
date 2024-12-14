3e9a1e771f557db5233f2038cadab3303b895bff
songziwen <songziwen03@meituan.com>
2024-12-14 16:14:57 +0800

    feat: 登陆注册页面完善

--- src/router/register/register.tsx
+++ src/router/register/register.tsx
@@ -1,33 +1,145 @@
-import React, { useState } from "react";
-import { LoginForm } from "./components/LoginForm";
-import { RegisterForm } from "./components/RegisterForm";
-
+import { useState } from "react";
 import "./register.less";
+import { postRequest } from "../../utils/api";
+import { useNavigate } from "react-router-dom";
 
-const LoginRegister: React.FC = () => {
-  const [activeTab, setActiveTab] = useState(0);
+export default function Register() {
+  // 注册状态
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  // 登录状态
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  // 注册提交处理
+  const handleRegisterSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const data = await postRequest("/api/v1/user/register", {
+      username,
+      email,
+      password,
+    });
+    setActiveTab("login");
+    setUsername("");
+    setEmail("");
+    setPassword("");
+    console.log(data, "注册成功");
+  };
+
+  // 登录提交处理
+  const handleLoginSubmit = async (e: React.FormEvent) => {
+    try {
+      e.preventDefault();
+      const data = await postRequest("/api/v1/user/login", {
+        username: loginUsername,
+        password: loginPassword,
+      });
+      console.log(data, "登录成功");
+      setLoginUsername("");
+      setLoginPassword("");
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const [activeTab, setActiveTab] = useState<"register" | "login">("register");
+
+  const handleTabChange = (tab: "register" | "login") => {
+    setActiveTab(tab);
+  };
 
   return (
-    <div className="container">
-      <div className="card">
-        <div className="tab-container">
-          <button
-            className={`tab-button ${activeTab === 0 ? "active" : ""}`}
-            onClick={() => setActiveTab(0)}
-          >
-            Login
-          </button>
-          <button
-            className={`tab-button ${activeTab === 1 ? "active" : ""}`}
-            onClick={() => setActiveTab(1)}
-          >
-            Register
-          </button>
-        </div>
-        {activeTab === 0 ? <LoginForm /> : <RegisterForm />}
+    <div className="register-container">
+      <div className="toggle-container">
+        <button
+          className={`toggle-button ${
+            activeTab === "register" ? "active" : ""
+          }`}
+          onClick={() => handleTabChange("register")}
+        >
+          注册
+        </button>
+        <button
+          className={`toggle-button ${activeTab === "login" ? "active" : ""}`}
+          onClick={() => handleTabChange("login")}
+        >
+          登录
+        </button>
       </div>
+
+      {activeTab === "register" ? (
+        <>
+          <h1 className="title">注册</h1>
+          <form className="register-form" onSubmit={handleRegisterSubmit}>
+            <div className="input-group">
+              <input
+                type="text"
+                className="input"
+                placeholder="用户名"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+            <div className="input-group">
+              <input
+                type="email"
+                className="input"
+                placeholder="邮箱"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="input-group">
+              <input
+                type="password"
+                className="input"
+                placeholder="密码"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit" className="register-button">
+              注册
+            </button>
+          </form>
+        </>
+      ) : (
+        <>
+          <h1 className="title">登录</h1>
+          <form className="login-form" onSubmit={handleLoginSubmit}>
+            <div className="input-group">
+              <input
+                type="text"
+                className="input"
+                placeholder="用户名"
+                value={loginUsername}
+                onChange={(e) => setLoginUsername(e.target.value)}
+                required
+              />
+            </div>
+            <div className="input-group">
+              <input
+                type="password"
+                className="input"
+                placeholder="密码"
+                value={loginPassword}
+                onChange={(e) => setLoginPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit" className="login-button">
+              登录
+            </button>
+          </form>
+        </>
+      )}
     </div>
   );
-};
-
-export default LoginRegister;
+}
